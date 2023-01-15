1e51897cc888b55db8e4f6fad4c8ce7cd8f1665a
Leon Forsythe <lvforsythe95@gmail.com>
2023-01-15 15:52:11 -0500

    added friend route and controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -45,12 +45,33 @@ module.exports = {
     }
   },
   //delete specific user
-  async deleteUser(req, res){
+  async deleteUser(req, res) {
     try {
-      const deletedUser = await User.findOneAndDelete({_id: req.params.id})
+      const deletedUser = await User.findOneAndDelete({ _id: req.params.id });
       res.status(200).json(deletedUser);
     } catch (error) {
-      res.status(500).json(error)
+      res.status(500).json(error);
     }
-  }
+  },
+  //add friend
+  async friend(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userid },
+        { $addToSet: { friends: req.params.friendid } },
+        { new: true }
+      )
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+
+  //delete friend
+  async unfriend(req, res) {
+    try {
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,12 +1,16 @@
 const router = require('express').Router()
 
 // require controllers here
-const {getUsers, getUserById, createUser, updateUser, deleteUser} = require('../../controllers/userController')
+const {getUsers, getUserById, createUser, updateUser, deleteUser, friend, unfriend} = require('../../controllers/userController')
 
 // /api/users
 router.route('/').get(getUsers).post(createUser)
 
 // /api/users/:userid
 router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser)
+
+// /api/users/:userid/friends/:friendid
+
+router.route('/:userid/friends/:friendid').post(friend).delete(unfriend)
 
 module.exports = router
